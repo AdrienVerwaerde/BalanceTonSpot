0155595944790ac4bba576d8892034963f106bb7
DiegoGumpel <gumpel.diego@gmail.com>
2024-02-05 12:06:14 +0100

    Carousel & SideBar in progress

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,54 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import { Image } from 'semantic-ui-react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import { Image, Segment, Header, Icon, Menu, MenuItem, Sidebar, Grid } from 'semantic-ui-react';
 
-const HeaderWrapper = styled.div`
-top: 0;
-width: 100%;
-background-color: #214c5c;
-color: white;
-padding: 10px;
-transition: transform 0.3s;
-z-index: 1000;
-`;
+const HeaderWithSidebar = () => {
+  const [sidebarVisible, setSidebarVisible] = useState(false);
 
-
-const Header = () => {
-  const [scrollY, setScrollY] = useState(0);
-
-  const handleScroll = () => {
-    const headerWrapper = document.querySelector('#header-wrapper');
-    if (headerWrapper) {
-      const scrollTop = headerWrapper.scrollTop || document.documentElement.scrollTop;
-      setScrollY(scrollTop);
-    }
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
   };
 
-  useEffect(() => {
-    const headerWrapper = document.querySelector('#header-wrapper');
-    
-    if (headerWrapper) {
-      headerWrapper.addEventListener('scroll', handleScroll);
-    }
-
-    return () => {
-      if (headerWrapper) {
-        headerWrapper.removeEventListener('scroll', handleScroll);
-      }
-    };
-  }, []);
-
   return (
-    <HeaderWrapper>
-      <img src="../../assets/images/logo.png"></img>
-      <div>
-      <Image src='../../assets/images/Adrien.png' avatar />
-      <span>Bonjour, Adrien Verwaerde</span>
-      </div>
-      <button>Unwrap</button>
-      <button>Skate</button>
-      <button>Snow</button>
-    </HeaderWrapper>
+    <div>
+      <Segment>
+        <Grid columns={3} stackable>
+          <Grid.Column width={4}>
+            <Image src="../../assets/images/logo.png" />
+          </Grid.Column>
+
+          <Grid.Column width={8} textAlign="center">
+            <Header as="h2">Your Header Title</Header>
+          </Grid.Column>
+
+          <Grid.Column width={4} textAlign="right">
+            <Image src="../../assets/images/Adrien.png" avatar />
+            <span>Bonjour, Adrien Verwaerde</span>
+            <button onClick={toggleSidebar}>Toggle Sidebar</button>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+
+      <Sidebar.Pushable>
+        <Sidebar
+          as={Menu}
+          animation='overlay'
+          icon='labeled'
+          inverted
+          onHide={() => setSidebarVisible(false)}
+          vertical
+          visible={sidebarVisible}
+          width='thin'
+          style={{
+            height: '100%',
+            top: 0,
+            left: 0,
+            position: 'fixed',
+            transform: `translateY(${sidebarVisible ? '0%' : '-100%'})`,
+            transition: 'transform 0.3s ease-in-out',
+          }}
+        >
+          <MenuItem as='a'>
+            <Icon name='home' />
+            Home
+          </MenuItem>
+          <MenuItem as='a'>
+            <Icon name='gamepad' />
+            Games
+          </MenuItem>
+          <MenuItem as='a'>
+            <Icon name='camera' />
+            Channels
+          </MenuItem>
+        </Sidebar>
+
+        <Sidebar.Pusher dimmed={sidebarVisible}>
+          <Segment basic>
+            {/* Your main content goes here */}
+            <Header as='h3'>Application Content</Header>
+            <Image src='/images/wireframe/paragraph.png' />
+          </Segment>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    </div>
   );
 };
-export default Header;
+
+export default HeaderWithSidebar;
