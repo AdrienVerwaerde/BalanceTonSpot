a033a52c7127451f713b5ecbf2ca4a0faca4d5fb
DiegoGumpel <gumpel.diego@gmail.com>
2024-02-06 12:12:46 +0100

    Slideshow finished

--- src/Components/Slideshow/Slideshow.tsx
+++ src/Components/Slideshow/Slideshow.tsx
@@ -3,9 +3,9 @@ import './Slideshow.css'
 
 const picture = ["https://cdn-s-www.ledauphine.com/images/0B4C75D1-BE1B-47ED-9CDF-B171D74277BD/NW_raw/le-snowpark-de-vars-s-etale-sur-plus-de-1-000-metres-de-denivele-c-est-ce-qui-fait-sa-singularite-et-sa-notoriete-qui-depassent-aujourd-hui-les-frontieres-europeennes-1390340766.jpg", "https://static.savoie-mont-blanc.com/wp-content/uploads/external/e132d5d4d725e4a69beabf7bcc818ecf-3800129-1745x1163.jpg", "https://www.laclusaz.com/app/uploads/apidae/7138618-diaporama-890x500.jpg"];
 
-const textSpotlight = ["Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat cum expedita, aspernatur accusamus, quasi voluptatem dignissimos officiis a dolor veritatis, voluptate perspiciatis molestias? Illum dignissimos nemo delectus dolor autem est", "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat cum expedita, aspernatur accusamus, quasi voluptatem dignissimos officiis a dolor veritatis, voluptate perspiciatis molestias? Illum dignissimos nemo delectus dolor autem est", "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat cum expedita, aspernatur accusamus, quasi voluptatem dignissimos officiis a dolor veritatis, voluptate perspiciatis molestias? Illum dignissimos nemo delectus dolor autem est"];
+const textSpotlight = ["Le Vars Park est certainement le meilleur snowpark de France mais bon, je suis pas objectif, c'est là que j'ai mes meilleurs souvenirs de snowboard", "Alors ce park là j'y suis jamais allé mais comme il y'a un skieur sur la photo j'imagine que c'est un spot de seconde zone mal fréquenté !", "La Clusaz, la Clusaz... on en entend beaucoup parler mais bon, c'est une station de riches ! Révolution populaire !!"];
 
-const delay = 5000;
+const delay = 4000;
 
 export default function Slideshow() {
   //useState SlideShow
@@ -37,10 +37,27 @@ export default function Slideshow() {
     };
   }, [index]);
 
+  //We create functions handleMouseEnter and handleMouseLeave that allow us to stop the carrousel when the mouse hovers it
+  function handleMouseEnter() {
+    resetTimeout();
+  }
+
+  function handleMouseLeave() {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === picture.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+  }
+
   return (
-    //!Slideshow
+    //! Slideshow
     //The div slideshow with the class slideshow who got the slideshowSlider with the transform translate3d and the slideshowDots
-    <div className="slideshow">
+    <div className="slideshow"  onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}>
 
       {/*
       The div slideshowSlider who got the img src with the picture
@@ -61,10 +78,12 @@ export default function Slideshow() {
             className="slide"
             key={index}
           >
-            <div id="spotlight">
-            <h2>SPOTLIGHT n° {index + 1}</h2>
-            <text id="spotlight-text"></text>
+            <a href="#"><div id="spotlight">
+            <h2>SPOTLIGHT N°{index + 1}</h2>
+            <text id="spotlight-text">{textSpotlight[index]}</text>
+            <p>Découvrir >></p>
             </div>
+            </a>
             <img src={p} alt="" />
           </div>
         ))}
